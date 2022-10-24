067488e69d679cc3fb7378f280d9ec96e6c7c625
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-10-23 21:25:44 -0400

    updates bg color and text color

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Header from '../components/Header'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='bg-neutral-900 text-white h-screen'>
       <Head>
         <title>Andrew&apos;s Portfolio</title>
       </Head>
       
       <Header />
+
+      <section id='hero'>
+        <Hero />
+      </section>
     </div>
   )
 }
