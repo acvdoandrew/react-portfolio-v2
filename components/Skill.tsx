9a17912894e6952c2493c5c39841cdc14c53a941
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-10-25 17:49:31 -0400

    sets up skills and projects sections

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -10,10 +10,10 @@ export default function Skill({ directionLeft }: Props) {
     <div>
         <motion.img 
             initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
-            whileInView={{ opacity: 1, x: 0}}
-            transition={{ duration: 1 }}
+            transition={{ duration: 0.5 }}
+            whileInView={{ opacity: 1, x: 0 }}
             src='https://media.glassdoor.com/sqll/4747101/sanity-ca-squareLogo-1640192856530.png'
-            className='rounded-full border border-gray-500 object-cover w-20 h-20 xl:w-32 xl:h-32'
+            className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32'
         />
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,27 +3,27 @@ import Head from 'next/head';
 import About from '../components/About';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import Projects from '../components/Projects';
 import Skills from '../components/Skills';
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-neutral-900 text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
+    <div className='bg-neutral-900 text-white h-screen w-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Andrew&apos;s Portfolio</title>
       </Head>
-      
       <Header />
-
       <section id='hero' className='snap-start'>
         <Hero />
       </section>
-
       <section id='about' className='snap-center'>
         <About />
       </section>
-
       <section id='skills' className='snap-start'>
         <Skills />
+      </section>
+      <section id='projects' className='snap-start'>
+        <Projects />
       </section>
     </div>
   )
