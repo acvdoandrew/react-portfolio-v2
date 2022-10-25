1c3bfeadbdf680890e1a7d41fdfeddc15643da39
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-10-25 19:02:59 -0400

    makes a top jump button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 import About from '../components/About';
 import ContactMe from '../components/ContactMe';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Projects from '../components/Projects';
 import Skills from '../components/Skills';
+import andrew from '../public/andrew.jpeg';
 
 const Home: NextPage = () => {
   return (
@@ -29,6 +32,14 @@ const Home: NextPage = () => {
       <section id='contact' className='snap-start'>
         <ContactMe />
       </section>
+
+      <Link href='#hero'>
+        <footer className='flex items-center justify-center  sticky bottom-5 w-full cursor-pointer'>
+          <div className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer overflow-hidden'>
+            <Image src={andrew} alt='logo' layout='fill' objectFit='cover'/>
+          </div>
+        </footer>
+      </Link>
     </div>
   )
 }
