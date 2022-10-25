cfb5f0def83d94a848b0d699a8b409143e1089fb
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-10-25 18:36:38 -0400

    tweaks skill component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import About from '../components/About';
+import ContactMe from '../components/ContactMe';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Projects from '../components/Projects';
@@ -24,6 +25,9 @@ const Home: NextPage = () => {
       </section>
       <section id='projects' className='snap-start'>
         <Projects />
+      </section>
+      <section id='contact' className='snap-start'>
+        <ContactMe />
       </section>
     </div>
   )
