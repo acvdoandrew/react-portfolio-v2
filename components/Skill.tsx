43f7c4877e054534223042d6bd5f7c73f163386f
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-10-25 18:50:08 -0400

    changes animation direction for skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 type Props = {
-    directionLeft?: boolean;
+    directionUp?: boolean;
 };
 
-export default function Skill({ directionLeft }: Props) {
+export default function Skill({ directionUp }: Props) {
   return (
     <div>
         <motion.img 
-            initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
+            initial={{ y: directionUp ? -200 : 200, opacity: 0 }}
             transition={{ duration: 1 }}
-            whileInView={{ opacity: 1, x: 0 }}
+            whileInView={{ opacity: 1, y: 0 }}
             src='https://media.glassdoor.com/sqll/4747101/sanity-ca-squareLogo-1640192856530.png'
             className='rounded-full border border-gray-500 object-cover w-20 h-20 xl:w-32 xl:h-32'
         />
