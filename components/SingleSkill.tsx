c164f114efb055a6b7b4412aa178f074f193d62c
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-10-25 22:28:57 -0400

    fixes bug with naming

--- components/SingleSkill.tsx
+++ components/SingleSkill.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import { Skill } from '../typings';
 
 type Props = {
+    skill: Skill;
     directionUp?: boolean;
 };
 
-export default function Skill({ directionUp }: Props) {
+export default function SingleSkill({ directionUp, skill }: Props) {
   return (
     <div>
         <motion.img 
             initial={{ y: directionUp ? -200 : 200, opacity: 0 }}
             transition={{ duration: 1 }}
             whileInView={{ opacity: 1, y: 0 }}
-            src='https://media.glassdoor.com/sqll/4747101/sanity-ca-squareLogo-1640192856530.png'
+            src={skill?.image}
             className='rounded-full border border-gray-500 object-cover w-20 h-20 xl:w-32 xl:h-32'
         />
     </div>
